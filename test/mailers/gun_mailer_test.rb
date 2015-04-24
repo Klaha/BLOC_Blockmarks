2dc2456c3e6f97220a8179d1ef46b1dbeeffe9c3
Jonathan Martínez <klaha.77@gmail.com>
2015-04-24 00:12:58 +0000

    ActionMailer Working on C9

---
+++ test/mailers/gun_mailer_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class GunMailerTest < ActionMailer::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/mailers/previews/gun_mailer_preview.rb
@@ -0,0 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/gun_mailer
+class GunMailerPreview < ActionMailer::Preview
+
+end
