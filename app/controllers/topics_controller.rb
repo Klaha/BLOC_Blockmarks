49d5df8f78aee80ecdd525fb91d8b2892ff01402
Jonathan Martínez <klaha.77@gmail.com>
2015-05-02 02:03:20 +0000

    Topics CRUD

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,10 +1,35 @@
 class TopicsController < ApplicationController
+  
+  def index
+    @topics = Topic.all
+  end
+  
+  def show
+    @topic = Topic.find(params[:id])
+    @bookmarks = @topic.bookmarks
+  end
+  
+  def new
+    @topic = Topic.new
+  end
 
   def create
-      @user = current_user
-      @topic = @user.topics.build(params.require(:topic).permit(:title))
-      
-      @topic.save
+    @user = current_user
+    @topic = @user.topics.build(params.require(:topic).permit(:title))
+    @topic.save
+    
+    redirect_to @topic
+  end
+  
+  def update
+  end
+  
+  def destroy
+    @topic = Topic.find(params[:id])
+    @topic.destroy
+    
+    redirect_to topics_path
+    
   end
   
 end
