6b8bb8fe9f576d70bd4ef69e9b10e1423023c80c
Jonathan Martínez <klaha.77@gmail.com>
2015-05-03 04:41:31 +0000

    Bookmarks CRUD

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,10 +1,29 @@
 class BookmarksController < ApplicationController
+  
+  def new
+    @topic = Topic.find(params[:topic_id])
+    @bookmark = Bookmark.new
+  end
 
   def create
       @topic = Topic.find(params[:topic_id])
       @bookmark = @topic.bookmarks.build(params.require(:bookmark).permit(:url))
       
       @bookmark.save
+      redirect_to @topic
+  end
+  
+  def show
+    @topic = Topic.find(params[:topic_id])
+    @bookmarks = Bookmark.find(params[:id])
+  end
+  
+  def destroy
+    @topic = Topic.find(params[:topic_id])
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    
+    redirect_to @topic
   end
   
 end
